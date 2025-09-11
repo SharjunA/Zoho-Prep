5d315aef8ef2036bb5a50d14670e0601bb663e95
xstratum-Sharjun <sharjun.a@xstratum.ai>
2025-09-11 20:11:00 +0530

    .

--- zoho_java_logic_solutions/section6_lld/library/Main.java
+++ zoho_java_logic_solutions/section6_lld/library/Main.java
@@ -4,10 +4,10 @@ public class Main {
 
         lib.addBook(new Book("978-0-12", "DSA in Java"));
         lib.addBook(new Book("978-0-34", "Clean Code"));
-        
+
         System.out.println("Books: " + lib.list());
         System.out.println("Checkout Clean Code: " + lib.checkout("978-0-34", "M1"));
         System.out.println("Books: " + lib.list());
         System.out.println("Return: " + lib.checkin("978-0-34"));
     }
-}
+}  
